3a3c918c5eb8ba641d86a9ba9fc76c907e9b4a04
Intoy <intoynto@gmail.com>
2023-09-20 21:28:09 +0800

    add method snake in str

--- src/Support/Str.php
+++ src/Support/Str.php
@@ -5,6 +5,13 @@ namespace Intoy\HebatSupport;
 class Str
 {
     /**
+     * The cache of snake-cased words.
+     *
+     * @var array
+     */
+    protected static $snakeCache = [];
+    
+    /**
      * Return the remainder of a string after the first occurrence of a given value.
      *
      * @param  string  $subject
@@ -409,6 +416,30 @@ class Str
         return trim($title, $separator);
     }
 
+
+    /**
+     * Convert a string to snake case.
+     *
+     * @param  string  $value
+     * @param  string  $delimiter
+     * @return string
+     */
+    public static function snake($value, $delimiter = '_')
+    {
+        $key = $value;
+
+        if (isset(static::$snakeCache[$key][$delimiter])) {
+            return static::$snakeCache[$key][$delimiter];
+        }
+
+        if (! ctype_lower($value)) {
+            $value = preg_replace('/\s+/u', '', ucwords($value));
+
+            $value = static::lower(preg_replace('/(.)(?=[A-Z])/u', '$1'.$delimiter, $value));
+        }
+
+        return static::$snakeCache[$key][$delimiter] = $value;
+    }
 
     /**
      * Determine if a given string starts with a given substring.
